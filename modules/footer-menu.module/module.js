005eb45c4fc986108962a9e07251bf490332f043
Dimitrije Stanković <dimitrije.stanco@gmail.com>
2025-03-21 09:57:17 +0100

    Edit Spa Baths Page Template Content, remove console logs

--- modules/footer-menu.module/module.js
+++ modules/footer-menu.module/module.js
@@ -3,10 +3,7 @@ const footerMenuParentItems = document.querySelectorAll('.footer_menu__item--has
 if(footerMenuParentItems) {
   footerMenuParentItems.forEach((item)=> {
     item.addEventListener('click', ()=> {
-      console.log(item);
       const submenu = item.querySelector('.footer_menu__submenu');
-      console.log(submenu);
-      
       submenu.classList.toggle('hidden')
       submenu.classList.toggle('flex')
     })
